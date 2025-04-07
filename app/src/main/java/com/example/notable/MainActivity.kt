f65fdeb64ea9569aeeffb8227de65f5c31b69a36
Parsa Ghezelbash <119513155+ParsaGhezelbash@users.noreply.github.com>
2025-04-07 20:31:49 +0330

    ui package test

--- app/src/main/java/com/example/notable/MainActivity.kt
+++ app/src/main/java/com/example/notable/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.notable
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.notable.login.Login
+import com.example.notable.onboarding.Onboarding
 import com.example.notable.ui.theme.NotableTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NotableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Onboarding(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        Toast.makeText(
+                            applicationContext,
+                            "Run Ahmadi",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotableTheme {
-        Greeting("Android")
     }
 }

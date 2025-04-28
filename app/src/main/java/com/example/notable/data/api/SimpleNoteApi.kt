1f9f45257f57a08fa7d77ee9a52cc1aba8f913ff
kasrahmi <a.kasrahmi@gmail.com>
2025-04-28 18:51:40 +0330

    Auth API added

--- app/src/main/java/com/example/notable/data/api/SimpleNoteApi.kt
+++ app/src/main/java/com/example/notable/data/api/SimpleNoteApi.kt
@@ -1,7 +1,13 @@
 package com.example.notable.data.api
+import retrofit2.http.*
 
 interface SimpleNoteApi {
-
-
-
+    @POST("api/auth/register")
+    suspend fun register(@Body body: RegisterRequest): RegisterResponse
+    @POST("api/auth/token")
+    suspend fun tokenCreate(@Body body: AuthTokenRequest): AuthTokenResponse
+    @POST("api/auth/token/refresh")
+    suspend fun tokenRefresh(@Body body: RefreshTokenRequest): RefreshTokenResponse
+    @GET("auth/userinfo/")
+    suspend fun getUserInfo(@Header("Authorization") authToken: String): UserInfoResponse
 }
